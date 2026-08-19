97862f267ee8415e0dedaaaff22c0a9ca98d1066
An Le <anle070205@gmail.com>
2026-08-19 17:41:15 +0700

    update and edit BT

---
+++ BT10.py
@@ -0,0 +1,4 @@
+s = "anh thật sự nhớ em rất nhiều \n"
+s = s.upper()
+s= s*10
+print(s )
--- BT9.py
+++ BT9.py
@@ -1,4 +1,5 @@
-for i in range(10):
-    print("Bảng cửu chương của {}".format(i+1))
-    for j in range(10):
-        print("{0} x {1} = {2}".format(i+1, j+1, (i+1)*(j+1)))
+# in bảng cửu chương
+for i in range(1, 11):
+    print("Bảng cửu chương của {}".format(i))
+    for j in range(1, 11):
+        print("{0} x {1} = {2}".format(i, j, i*j))
